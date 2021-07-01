daeedb70d355d79d8d01db60ca80aeeb0ff84736
Nell-ok <nelli-nn@yandex.ru>
2021-07-01 10:36:08 +0300

    небольшие исправления

--- js/catalog.js
+++
@@ -1,4 +0,0 @@
-const catalogFilters = document.querySelector('.catalog-filters')
-catalogFilters.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-});
--- js/search.js
+++ js/search.js
@@ -1,9 +1,9 @@
 const formSearch = document.querySelector('.search-form');
 const buttonSearch = document.querySelector('.button-search');
-const DateIn = formSearch.querySelector('[name=date-in]');
-const DateOut = formSearch.querySelector('[name=date-out]');
-const ControlBig = formSearch.querySelector('[name=control-big]');
-const ControlLittle = formSearch.querySelector('[name=control-little]');
+const dateIn = formSearch.querySelector('[name=date-in]');
+const dateOut = formSearch.querySelector('[name=date-out]');
+const controlBig = formSearch.querySelector('[name=control-big]');
+const controlLittle = formSearch.querySelector('[name=control-little]');
 
 let isStorageSupport = true;
 let storage = "";
@@ -17,17 +17,19 @@ try {
 
 formSearch.classList.add('form-hidden');
 
-buttonSearch.onclick = function () {
+buttonSearch.addEventListener('click', function () {
   formSearch.classList.toggle('form-show');
-  DateIn.focus();
-};
+  if(formSearch.classList.contains('form-show')) {
+    dateIn.focus();
+  }
+});
 formSearch.addEventListener('submit', function (evt) {
-  if (!DateIn.value || !DateOut.value || !ControlBig.value || !ControlLittle.value) {
+  if (!dateIn.value || !dateOut.value || !controlBig.value || !controlLittle.value) {
     evt.preventDefault();
   } else {
     if (isStorageSupport) {
-      localStorage.setItem('control-big', ControlBig.value);
-      localStorage.setItem('control-little', ControlLittle.value);
+      localStorage.setItem('control-big', controlBig.value);
+      localStorage.setItem('control-little', controlLittle.value);
     }
   }
 });
